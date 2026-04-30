782c722e0c879691217ee5df9870576593659990
KevinLikesCoding <cyf52mc@163.com>
2026-04-30 23:28:14 +0800

    feat: add mino.h and its test

--- def.h
+++ def.h
@@ -5,4 +5,9 @@
 #ifndef KEROSHIZEROTETRIS_DEF_H
 #define KEROSHIZEROTETRIS_DEF_H
 
+#include <cstdint>
+#include <random>
+#include <algorithm>
+#include <iostream>
+
 #endif //KEROSHIZEROTETRIS_DEF_H
---
+++ test/test_mino.cpp
@@ -0,0 +1,38 @@
+//
+// Created by keroshi on 2026/4/30.
+//
+
+#include "def.h"
+#include "mino.h"
+
+#include <thread>
+#include <chrono>
+
+int a[5][5];
+
+int main() {
+	int r = 0;
+	for (int piece = 1; piece <= 7; piece ++) {
+		for (int _ = 0; _ < 8; _ ++) {
+			std :: cout << "\033[2J\033[H" << std :: flush;
+			for (int x = 0; x < 5; x ++)
+				for (int y = 0; y < 5; y ++)
+					a[x][y] = 0;
+			for (int i = 0; i < 4; i ++) {
+				int x = 3 + Mino :: minos[piece][r][i][0];
+				int y = 3 + Mino :: minos[piece][r][i][1];
+				a[x][y] = 1;
+			}
+			for (int y = 4; y >= 0; y --) {
+				for (int x = 0; x < 5; x ++) {
+					std :: cout << a[x][y];
+				}
+				std :: cout  << std :: endl;
+			}
+			std :: this_thread :: sleep_for(std :: chrono :: milliseconds(500));
+			r = (r + 1) & 3;
+		}
+	}
+	return 0;
+}
+
