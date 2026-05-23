46b62aa0279633a06024ab96af42efeb2785c6b0
KevinLikesCoding <cyf52mc@163.com>
2026-05-23 19:50:34 +0800

    refactor: split converter to header and cpp

--- include/converter.h
+++ include/converter.h
@@ -9,69 +9,18 @@
 
 #include <ATen/ATen.h>
 
-struct InputConverter {
+namespace InputConverter {
 
-	static at :: Tensor to_board(const Tetris & t) {
-		auto tensor = at :: zeros({1, 1, 30, 10}, at :: kFloat);
-		auto acc = tensor.accessor<float, 4>();
-		for (int i = 0; i < 30; i ++) for (int j = 0; j < 10; j ++) {
-			if (t.b[i] >> j & 1) acc[0][0][i][j] = 1.f;
-		}
-		return tensor;
-	}
+	at :: Tensor to_board(const Tetris & t);
 
-	static at :: Tensor to_seq(const Tetris & t) {
-		auto tensor = at :: zeros({1, 10}, at :: kLong);
-		auto acc = tensor.accessor<int64_t, 2>();
-		acc[0][0] = static_cast<int64_t>(t.cur);
-		acc[0][1] = static_cast<int64_t>(t.hold);
-		for (int i = 0; i < 5; i ++) {
-			acc[0][i + 2] = static_cast<int64_t>(t.nxt[i]);
-		}
-		return tensor;
-	}
+	at :: Tensor to_seq(const Tetris & t);
 
-	static at :: Tensor to_info(const Tetris & t) {
-		auto tensor = at :: zeros({1, 10}, at :: kFloat);
-		auto acc = tensor.accessor<float, 2>();
-		acc[0][0] = t.can_hold ? 1.f : 0.f;
-		acc[0][1] = t.combo;
-		acc[0][2] = t.b2b;
-		acc[0][3] = t.pieces;
-		acc[0][4] = t.attack;
-		return tensor;
-	}
+	at :: Tensor to_info(const Tetris & t);
 
-	static at :: Tensor to_pos(const std :: vector<int> & actions, int cur) {
-		int n = actions.size();
-		auto tensor = at :: zeros({n, 4}, at :: kFloat);
-		auto acc = tensor.accessor<float, 2>();
-		for (int i = 0; i < n; i ++) {
-			int u = actions[i];
-			if (u == 0) {
-				acc[i][0] = 0;
-				acc[i][1] = 0;
-				acc[i][2] = 0;
-				acc[i][3] = 0;
-			}
-			else {
-				auto [x, y, r] = Action :: decode(u);
-				acc[i][0] = x;
-				acc[i][1] = y;
-				acc[i][2] = r;
-				acc[i][3] = cur;
-			}
-		}
-		return tensor;
-	}
-	static at :: Tensor to_pos(Tetris & t) {
-		return to_pos(t.legal(), static_cast<int>(t.cur));
-	}
+	at :: Tensor to_pos(const std :: vector<int> & actions, int cur);
+	at :: Tensor to_pos(Tetris & t);
 
-	static at :: Tensor to_offset(const at :: Tensor & pos) {
-		int64_t n = pos.size(0);
-		return at :: tensor({n}, at :: kLong);
-	}
+	at :: Tensor to_offset(const at :: Tensor & pos);
 };
 
 #endif //KEROSHIZEROTETRIS_CONVERTER_H
