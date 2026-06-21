878f832daf27bbbc060a83ee480106fcb3123421
Keroshi <cyf52mc@163.com>
2026-06-21 15:41:47 +0800

    feat: script to set TETR.IO piece sequence

---
+++ api/io_seq.js
@@ -0,0 +1,6 @@
+function KeroshiSetSeq(seq_str) {
+    seq = Array.from(seq_str.toLowerCase().trim());
+    window.keroshi_S.falling.type = seq[0];
+    seq.shift();
+    window.keroshi_S.bag = seq;
+}
