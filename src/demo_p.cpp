3e5ec5827923b2f7a7db4d2ee3cb7dc7522c8209
Keroshi <cyf52mc@163.com>
2026-07-15 22:16:59 +0800

    feat: write more information to files

--- src/demo_p.cpp
+++ src/demo_p.cpp
@@ -63,7 +63,7 @@ void demo_p(const std :: string & model_path, int simu) {
 
 			auto [V, P] = predictor.predict(g);
 
-			std :: cout << V << std :: endl;
+			std :: cout << tetris.attack << ' ' << V << std :: endl;
 
 			fst = get_V(g) + V;
 		}
@@ -93,6 +93,106 @@ void demo_p(const std :: string & model_path, int simu) {
 	std :: cout << tetris.attack << std :: endl;
 }
 
+void demo_p(const std :: string & model_path, int simu, const std :: string & output_path) {
+
+	std :: ofstream ofs(output_path);
+
+	int height = 10;
+	ofs << height << std :: endl;
+
+	Predictor predictor(
+		model_path,
+		true, true
+	);
+
+	Tetris tetris;
+	tetris.rest_pieces = 12;
+
+	while (tetris.rest_pieces > 0) {
+		if (is_end(tetris)) break;
+
+		std :: vector<std :: string> tb(height);
+
+		for (int y = height - 1; y >= 0; y --) {
+			for (int x = 0; x < 10; x ++) {
+				int o = tetris.b[y] >> x & 1;
+				ofs << "_X"[o];
+				tb[y] += "_X"[o];
+			}
+			ofs << std :: endl;
+		}
+
+		MCTS<Tetris> mcts;
+		mcts.C = 7;
+
+		float fst;
+
+		{
+			Tetris g = tetris;
+			mcts.root_game = tetris;
+			mcts.root_game.depleted = true;
+			mcts.root_game.record_seq = false;
+
+			auto [V, P] = predictor.predict(g);
+
+			fst = get_V(g) + V;
+		}
+
+		for (int K = 0; K < simu; K ++) mcts_single(mcts, predictor, fst);
+
+		int id = mcts.get_best();
+		int u = mcts.root -> a[id];
+
+		int piece = static_cast<int>(tetris.cur);
+		tetris.step(u);
+
+		int num = 3;
+		ofs << num << std :: endl;
+
+		int n = mcts.root -> n;
+		std :: vector<int> idx(n);
+		for (int i = 0; i < n; i ++) idx[i] = i;
+
+		auto mcts_N = mcts.root -> N;
+		std :: sort(idx.begin(), idx.end(), [&] (int x, int y) {
+			return mcts_N[x] > mcts_N[y];
+		});
+
+		for (int k = 0; k < num; k ++) {
+			int i = idx[k];
+			double p = (double) mcts_N[i] / mcts.root -> Sum_N;
+			ofs << (p * 100) << '%' << std :: endl;
+			int U = mcts.root -> a[i];
+			if (U == 0) {
+				ofs << "Hold" << std :: endl;
+			}
+			else {
+				std :: vector<std :: string> b(height);
+				for (int y = height - 1; y >= 0; y --) {
+					b[y] = tb[y];
+				}
+
+				auto [x, y, r] = Action :: decode(U);
+				char pc = "_IJLOSTZ"[piece];
+
+				for (int i = 0; i < 4; i ++) {
+					int X = x + Mino :: minos[piece][r][i][0];
+					int Y = y + Mino :: minos[piece][r][i][1];
+					b[Y][X] = pc;
+				}
+
+				ofs << "board" << std :: endl;
+				for (int y = height - 1; y >= 0; y --) {
+					ofs << b[y] << std :: endl;
+				}
+
+			}
+		}
+
+	}
+
+}
+
 int main(int argc, char * argv []) {
 	if (argc < 2) {
 		std :: cout << "Usage: " << argv[0]
@@ -118,7 +218,7 @@ int main(int argc, char * argv []) {
 
 	int simu = simu_number;
 
-	demo_p(model_path, simu);
+	demo_p(model_path, simu, "test.out");
 
 	return 0;
 }
