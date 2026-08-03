a877e2e02b237c6c388bca210385b8c8593fdd3a
KevinLikesCoding <cyf52mc@163.com>
2026-08-03 21:21:08 +0800

    test: Tetris step

---
+++ tests/tetris/random.cpp
@@ -0,0 +1,58 @@
+//
+// Created by keroshi on 2026/8/3.
+//
+
+#include "include/core/tetris.hpp"
+#include "include/ui/tetris_ftxui.hpp"
+
+#include <random>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
+
+
+int main() {
+	ftxui :: Elements boards;
+
+	std :: mt19937 rnd(std :: random_device {} ());
+
+	TetrisEnv tetris;
+	TetrisEnvView view(tetris);
+
+	auto step = [&] () {
+		if (tetris.is_over()) return;
+
+		bool success = false;
+
+		if (tetris.is_player()) {
+			auto actions = tetris.get_actions();
+			int u = rnd() % actions.size();
+			success = tetris.step_player(actions[u]);
+		}
+		else {
+			auto actions = tetris.get_gen_actions();
+			int u = rnd() % actions.size();
+			success = tetris.step_gen(actions[u]);
+		}
+
+		if (! success) {
+			std :: abort();
+		}
+	};
+
+	auto last = std :: chrono :: steady_clock :: now();
+	auto renderer = ftxui :: Renderer([&] {
+
+		auto now = std :: chrono :: steady_clock :: now();
+		if (now - last >= std :: chrono :: milliseconds(200)) {
+			step();
+			last = now;
+		}
+
+		return view.render();
+	});
+
+	auto screen = ftxui :: ScreenInteractive :: TerminalOutput();
+	screen.Loop(renderer);
+
+	return 0;
+}
