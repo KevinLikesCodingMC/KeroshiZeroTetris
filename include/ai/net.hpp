a8ac8ca59cad8f99c319a4c1cc70dc1f55ebf4bd
KevinLikesCoding <cyf52mc@163.com>
2026-08-12 11:03:11 +0800

    feat: load optimizer

--- include/ai/net.hpp
+++ include/ai/net.hpp
@@ -6,6 +6,7 @@
 #define KEROSHIZEROTETRIS_NET_HPP
 
 #include "include/core/tetris.hpp"
+#include "dataset.hpp"
 
 #include <torch/torch.h>
 #include <torch/script.h>
@@ -33,6 +34,22 @@ public:
 	);
 
 	float predict(TetrisEnv & tetris);
+};
+
+class TetrisValueTrainNet : public TetrisValueNet {
+public:
+	TetrisValueTrainNet(
+		const std :: string & model_path,
+		const std :: string & opt_path,
+		bool cuda
+	);
+
+	float train(const std :: vector<Dataset :: player_value> & data);
+
+	void save(const std :: string & path);
+
+private:
+	std :: unique_ptr<torch :: optim :: Adam> optimizer;
 };
 
 #endif //KEROSHIZEROTETRIS_NET_HPP
--- src/ai/net.cpp
+++ src/ai/net.cpp
@@ -36,6 +36,8 @@ TetrisNet :: TetrisNet(const std :: string & path, bool cuda)
 
 		module.to(device);
 		module.eval();
+
+		Logger :: info("model <", path, "> loaded.");
 	}
 	catch (const c10 :: Error & e) {
 		Logger :: error("model <", path, "> loading failed.");
@@ -75,4 +77,54 @@ float TetrisValueNet :: predict(TetrisEnv & tetris) {
 	auto output = predict(board, piece, info);
 
 	return output.cpu().item<float>();
+}
+
+TetrisValueTrainNet :: TetrisValueTrainNet(
+	const std :: string & model_path,
+	const std :: string & opt_path,
+	bool cuda
+) : TetrisValueNet(model_path, cuda) {
+
+	std :: vector<torch :: Tensor> params;
+	for (at :: Tensor p : module.parameters()) {
+		(void) p.set_requires_grad(true);
+		params.push_back(p);
+	}
+
+	optimizer = std :: make_unique<torch :: optim :: Adam>(
+		params,
+		torch :: optim :: AdamOptions(1e-3)
+	);
+
+	if (opt_path.empty() || ! std :: filesystem :: exists(opt_path)) {
+		Logger :: info("Initialized default optimizer.");
+	}
+	else {
+		try {
+			torch :: load(* optimizer, opt_path);
+			Logger :: info("optimizer <", opt_path, "> loaded.");
+		} catch (const c10 :: Error & e) {
+			Logger :: error("optimizer <", opt_path, "> loading failed.");
+			throw std :: runtime_error(e.what());
+		}
+	}
+
+}
+
+void TetrisValueTrainNet :: save(const std :: string & path) {
+	try {
+		module.save(path + ".pt");
+		Logger :: info("model <", path + ".pt", "> saved.");
+	} catch (const c10 :: Error & e) {
+		Logger :: error("model <", path + ".pt", "> saving failed.");
+		throw std :: runtime_error(e.what());
+	}
+
+	try {
+		torch :: save(* optimizer, path + ".opt");
+		Logger :: info("optimizer <", path + ".opt", "> saved.");
+	} catch (const c10 :: Error & e) {
+		Logger :: error("optimizer <", path + ".opt", "> saving failed.");
+		throw std :: runtime_error(e.what());
+	}
 }
